39fb2a12b43dd2a13dbee984ef593105de885e5e
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-11-12 20:20:01 +0300

    Init commit

---
+++ lua/blotzartdd_nvim/core/options.lua
@@ -0,0 +1,48 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
+-- Rust
+vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true}
+vim.api.nvim_set_option('updatetime', 300) 
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error 
+-- Show inlay_hints more frequently 
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+-- Treesitter folding 
+-- vim.wo.foldmethod = 'expr'
+-- vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
---
+++ lua/blotzartdd_nvim/plugins/lsp/null-ls.lua
@@ -0,0 +1,61 @@
+-- import null-ls plugin safely
+local setup, null_ls = pcall(require, "null-ls")
+if not setup then
+  return
+end
+
+-- for conciseness
+local formatting = null_ls.builtins.formatting -- to setup formatters
+local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+
+-- to setup format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+
+
+-- configure null_ls
+null_ls.setup({
+  -- setup formatters & linters
+  sources = {
+    --  to disable file types use
+    --  "formatting.prettier.with({disabled_filetypes = {}})" (see null-ls docs)
+    formatting.clang_format,
+    -- diagnostics.clang_check,
+    diagnostics.chktex,
+    -- diagnostics.cppcheck,
+    diagnostics.cpplint,
+    diagnostics.pylint,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources, {
+        nls.builtins.formatting.clang_format.with({
+          extra_args = {
+            "-style=file:" .. vim.fn.expand("/path/to/.clang-format"),
+          },
+        }),
+      })
+    end,
+  },
+  -- configure format on save
+  on_attach = function(current_client, bufnr)
+    if current_client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            filter = function(client)
+              --  only use null-ls for formatting instead of lsp server
+              return client.name == "null-ls"
+            end,
+            bufnr = bufnr,
+          })
+        end,
+      })
+    end
+  end,
+})
